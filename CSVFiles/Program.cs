debd942c5789d7525db1fac454236663d1f3f290
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-05 11:26:19 -0300

    Add project files.

---
+++ CSVFiles/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVFiles {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
